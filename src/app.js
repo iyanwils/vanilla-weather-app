be8bf6295beb0b1ad04c0c94ae818550316def26
iyanwils <iyanawilson1996@gmail.com>
2021-08-25 14:23:30 -0400

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10){
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10){
+    minutes = `0${minutes}`;
+  }
+let days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response){
     let tempElement = document.querySelector('#temperature');
     tempElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +26,8 @@ function displayTemperature(response){
     humidityElement.innerHTML = response.data.main.humidity;
 let windElement = document.querySelector('#wind');
 windElement.innerHTML = Math.round(response.data.wind.speed);
+let dateElement = document.querySelector('#date');
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
