9b106a642584fdc820c4f327de1567b31cc1c325
iyanwils <iyanawilson1996@gmail.com>
2021-08-27 17:22:50 -0400

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ let windElement = document.querySelector('#wind');
 windElement.innerHTML = Math.round(response.data.wind.speed);
 let dateElement = document.querySelector('#date');
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+let iconElement = document.querySelector("#icon");
+iconElement.innerHTML.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}04d@2x.png`) ;
 }
 
 
