75fbc9124ffac9f337b789074326257f96589f0a
iyanwils <iyanawilson1996@gmail.com>
2021-08-29 17:42:00 -0400

    Improved my HTML and JS

--- src/app.js
+++ src/app.js
@@ -1,53 +1,54 @@
-function formatDate(timestamp){
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10){
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-let days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let day = days[date.getDay()];
 
   return `${day} ${hours}:${minutes}`;
 }
 
-
-function displayTemperature(response){
-    let tempElement = document.querySelector('#temperature');
-    tempElement.innerHTML = Math.round(response.data.main.temp);
-    let cityElement = document.querySelector('#city');
-    cityElement.innerHTML = response.data.name;
-    let descriptionElement = document.querySelector('#description');
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    let humidityElement = document.querySelector('#humidity');
-    humidityElement.innerHTML = response.data.main.humidity;
-let windElement = document.querySelector('#wind');
-windElement.innerHTML = Math.round(response.data.wind.speed);
-let dateElement = document.querySelector('#date');
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
-let iconElement = document.querySelector("#icon");
-iconElement.innerHTML.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}04d@2x.png`) ;
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}04d@2x.png`
+  );
 }
 
-function search(city){
-let apiKey = '361e0aa61c613a70c483b8ca214c4ec2';
-let city = "New York";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "361e0aa61c613a70c483b8ca214c4ec2";
+  let city = "New York";
+  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function handleSubmit(event){
-event.preventDefault();
-let cityInputElement = document.querySelector("#city-input");
-search(cityInputElement.value);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-
-
-let form = document.querySelector('#search-form');
-form.addEventListener('submit', handleSubmit);
+search("New York");
