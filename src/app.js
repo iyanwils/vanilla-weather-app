3790ccb996689428268bc6e40434d474cc321a9b
iyanwils <iyanawilson1996@gmail.com>
2021-09-04 16:16:52 -0400

    Added a footer and forecast

--- src/app.js
+++ src/app.js
@@ -53,3 +53,41 @@ function formatDate() {
 }
 
 let currentTime = new Date();
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+      <div class="weather-forecast-date">${formatDate(forecastDay)}</div>
+      <img src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
+      alt=""
+      width="42"
+      />
+      <div class="weather-forecast-temperatures">
+      <pan class="weather-forecast-temperature-max">${Math.round(
+        forecastDay.temp.max
+      )}°</span>
+      <span class="weather-forecast-temperature-min">${Math.round(
+        forecastDay.temp.min
+      )}°</span>
+      </div>
+      </div>
+      `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=361e0aa61c613a70c483b8ca214c4ec2&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
